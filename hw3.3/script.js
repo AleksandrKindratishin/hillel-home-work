3bbc00e8c3f281f4bd8a091e7554d4c9cc2a6e2a
Aleksandr Kindratishin <kindratishin24kindra@gmail.com>
2024-11-15 06:48:15 +0200

    added hw3.3

---
+++ hw3.3/script.js
@@ -0,0 +1,20 @@
+// Розкласти за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл. Приклад:
+//
+// 103691
+//
+// 1 0 3 6 9
+
+const someNumber = String(10369);
+
+const result = someNumber[0] +
+    ' ' +
+    someNumber[1] +
+    ' ' +
+    someNumber[2] +
+    ' ' +
+    someNumber[3] +
+    ' ' +
+    someNumber[4];
+
+console.log(result);
+
