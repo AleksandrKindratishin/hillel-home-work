c7637c20759be45713671e8e3edf29280f302966
Aleksandr Kindratishin <kindratishin24kindra@gmail.com>
2024-11-15 06:57:34 +0200

    added wh4.1

---
+++ hw4.1/script.js
@@ -0,0 +1,6 @@
+// За допомогою prompt запитати “ім'я користувача”.
+// За допомогою alert вивести "Hello, John! How are you?" , де “John” це те, що ввів користувач
+
+const userName = prompt('What is your name?', 'John');
+
+const helloMessage = alert('Hello, ' + userName + '!');
